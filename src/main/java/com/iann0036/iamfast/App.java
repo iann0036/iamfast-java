ae3540c7aafc8109d1e8b4f121ac2e1aef39f3c7
Ian Mckay <ian.mckay@kablamo.com.au>
2021-03-06 16:40:55 +1100

    Add sample

--- src/main/java/com/iann0036/iamfast/App.java
+++ src/main/java/com/iann0036/iamfast/App.java
@@ -2,19 +2,30 @@ package com.iann0036.iamfast;
 
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.VariableDeclarator;
+import java.io.File;
+import java.io.FileNotFoundException;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        CompilationUnit compilationUnit = StaticJavaParser.parse("class A {}");
-        
-        compilationUnit.findAll(FieldDeclaration.class).stream()
-            .filter(f -> f.isPublic() && !f.isStatic())
-            .forEach(f -> System.out.println("Check field at line" +
-                f.getRange().map(r -> r.begin.line).orElse(-1)));
+public class App {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Not enough arguments");
+            return;
+        }
 
-        System.out.println("TBC");
+        File file = new File(args[0]);
+
+        try {
+            parseFile(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("Couldn't file the specified file");
+        }
+    }
+
+    public static void parseFile(File file) throws FileNotFoundException {
+        CompilationUnit compilationUnit = StaticJavaParser.parse(file);
+
+        compilationUnit.findAll(VariableDeclarator.class).stream().forEach(
+                f -> System.out.println("Check var at line" + f.getRange().map(r -> r.begin.line).orElse(-1)));
     }
 }
--- src/test/java/com/iann0036/iamfast/AppTest.java
+++ src/test/java/com/iann0036/iamfast/AppTest.java
@@ -1,14 +1,28 @@
 package com.iann0036.iamfast;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class AppTest 
 {
     @Test
     public void shouldAnswerWithTrue()
     {
+        File file = new File("./testFiles/s3Op.java");
+
+        try {
+            App.parseFile(file);
+        } catch (FileNotFoundException e) {
+            fail(e.getMessage());
+        }
+
         assertTrue( true );
+
+        System.out.println("Done.");
     }
 }
