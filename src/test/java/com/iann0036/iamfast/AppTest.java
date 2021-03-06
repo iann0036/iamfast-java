6edf70a1f1a50cfb69eefe45d9b41201d786aa0f
Ian Mckay <ian.mckay@kablamo.com.au>
2021-03-06 16:49:47 +1100

    Another example

--- src/test/java/com/iann0036/iamfast/AppTest.java
+++ src/test/java/com/iann0036/iamfast/AppTest.java
@@ -1,6 +1,5 @@
 package com.iann0036.iamfast;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
@@ -11,18 +10,26 @@ import java.io.FileNotFoundException;
 public class AppTest 
 {
     @Test
-    public void shouldAnswerWithTrue()
+    public void s3CreateBucket()
     {
-        File file = new File("./testFiles/s3Op.java");
+        File file = new File("./testFiles/s3CreateBucket.java");
 
         try {
             App.parseFile(file);
         } catch (FileNotFoundException e) {
             fail(e.getMessage());
         }
+    }
 
-        assertTrue( true );
+    @Test
+    public void ec2StartInstance()
+    {
+        File file = new File("./testFiles/ec2StartInstance.java");
 
-        System.out.println("Done.");
+        try {
+            App.parseFile(file);
+        } catch (FileNotFoundException e) {
+            fail(e.getMessage());
+        }
     }
 }
