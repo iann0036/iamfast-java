a90c61d41de681f2563fb6f90032f7140af8bee1
Ian Mckay <ian.mckay@kablamo.com.au>
2021-03-07 13:02:22 +1100

    Minor updates

--- src/main/java/com/iann0036/iamfast/App.java
+++ src/main/java/com/iann0036/iamfast/App.java
@@ -2,9 +2,14 @@ package com.iann0036.iamfast;
 
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.ImportDeclaration;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.expr.VariableDeclarationExpr;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 
 public class App {
     public static void main(String[] args) {
@@ -25,11 +30,40 @@ public class App {
     public static void parseFile(File file) throws FileNotFoundException {
         CompilationUnit compilationUnit = StaticJavaParser.parse(file);
 
-        compilationUnit.findAll(VariableDeclarator.class).stream().forEach(
-            f -> {
-                System.out.println(f.getNameAsString());
-                System.out.println(f.getTypeAsString());
-            }
-        );
+        ArrayList<String> imports = new ArrayList<String>();
+        HashMap<String, String> clientMap = new HashMap<String, String>();
+        HashMap<String, String> requestMap = new HashMap<String, String>();
+
+        compilationUnit.findAll(ImportDeclaration.class).forEach(node -> {
+            imports.add(node.getNameAsString());
+        });
+
+        compilationUnit.findAll(VariableDeclarationExpr.class).forEach(node -> {
+            node.getVariables().forEach(v -> {
+                String name = v.getNameAsString();
+                String type = v.getTypeAsString();
+
+                Iterator<String> iter = imports.iterator();
+                while (iter.hasNext()) {
+                    String importItem = iter.next();
+                    if (importItem.endsWith("." + type) && type.endsWith("Client")) {
+                        clientMap.put(name, importItem);
+                    } else if (importItem.endsWith(".model." + type) && type.endsWith("Request")) {
+                        requestMap.put(name, importItem);
+                    }
+                }
+            });
+        });
+
+        compilationUnit.findAll(MethodCallExpr.class).forEach(node -> {
+            System.out.println(node);
+
+            System.out.println(node.getScope());
+            System.out.println(node.getName());
+            System.out.println(node.getTypeArguments());
+            System.out.println(node.getArguments());
+
+            System.out.println("---");
+        });
     }
 }
